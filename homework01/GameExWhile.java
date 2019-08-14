88f4d4481f7dd4ad6491d8d105f13286b0de97dd
hamuse <hamuseco@gmail.com>
2019-08-14 09:02:16 +0900

    190814 09:02

--- homework01/GameExWhile.java
+++ homework01/GameExWhile.java
@@ -1,45 +1,77 @@
 package homework01;
+
 import java.util.Random;
 import java.util.Scanner;
+
 public class GameExWhile {
 
 	public static void main(String[] args) {
-     while(true) {
-    	 
-    	 Scanner scan = new Scanner(System.in);
- 		Random ran = new Random();
- 		int num = ran.nextInt(3)+1;
+		while (true) {
 
- 		System.out.println("가위 바위 보를 입력하세요.");
- 		String user = scan.next();
- 		System.out.println(user);
- 		switch(num) {
- 			case 1 : System.out.println("컴퓨터:가위"); 
- 				switch(user) {
- 					case "가위" : System.out.println("비겼습니다.");break;
- 					case "바위" : System.out.println("이겼습니다");break;
- 					case "보" : System.out.println("졌습니다");break;
- 					
- 		}break;
- 			case 2 : System.out.println("컴퓨터:바위"); 
- 				switch(user) {
- 					case "가위" : System.out.println("졌습니다..");break;
- 					case "바위" : System.out.println("비겼습니다.");break;
- 					case "보" : System.out.println("이겼습니다.");break;
- 		}break;
- 			case 3: System.out.println("컴퓨터:보"); 
- 				switch(user) {
- 					case "가위" : System.out.println("이겼습니다...");break;
- 					case "바위" : System.out.println("졌습니다.");break;
- 					case "보" : System.out.println("비겼습니다.");
- 						break;
- 		}break;
- 			default : return;
- 		}
- 		System.out.println(num);
- 		
-      System.out.println("다시시작");
-     }
+			Scanner scan = new Scanner(System.in);
+			Random ran = new Random();
+			int num = ran.nextInt(3) + 1;
+
+			System.out.println("가위 바위 보를 입력하세요.");
+			String user = scan.next();
+			System.out.println(user);
+			switch (num) {
+			case 1:
+				System.out.println("컴퓨터:가위");
+				switch (user) {
+				case "가위":
+					System.out.println("비겼습니다.");
+					break;
+				case "바위":
+					System.out.println("이겼습니다");
+					break;
+				case "보":
+					System.out.println("졌습니다");
+					break;
+				default: System.out.println("입력오류");
+				return;
+
+				}
+				break;
+			case 2:
+				System.out.println("컴퓨터:바위");
+				switch (user) {
+				case "가위":
+					System.out.println("졌습니다..");
+					break;
+				case "바위":
+					System.out.println("비겼습니다.");
+					break;
+				case "보":
+					System.out.println("이겼습니다.");
+					break;
+				default: System.out.println("입력오류");
+				return;
+				}
+				break;
+			case 3:
+				System.out.println("컴퓨터:보");
+				switch (user) {
+				case "가위":
+					System.out.println("이겼습니다...");
+					break;
+				case "바위":
+					System.out.println("졌습니다.");
+					break;
+				case "보":
+					System.out.println("비겼습니다.");
+					break;
+				default: System.out.println("입력오류");
+				return;
+				}
+				break;
+			default: System.out.println("입력오류");
+				return;
+			}
+			System.out.println(num);
+
+			System.out.println("다시시작");
+		}
 	}
 
 }
